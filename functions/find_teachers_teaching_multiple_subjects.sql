ec78542009cba29f403cd0306f7f7b6a305fadc4
v203 <vuyisamr203@gmail.com>
2022-03-28 16:27:09 +0200

    completed all scripts

--- functions/find_teachers_teaching_multiple_subjects.sql
+++ functions/find_teachers_teaching_multiple_subjects.sql
@@ -1,25 +1,25 @@
 create or replace function find_teachers_teaching_multiple_subjects()
     returns table( 
-         id int,
          last_name text,
          first_name text,
-         email text,
+         subject_count bigint
+        --  email text
          
     )
         as $$
-        declare our_count
+        declare our_count int;
             begin
-
+-- count(*)
         return query 
-         select 
- teacher.*,count(subject.name) into our_count
-from teacher
-join teacher_subject on "teacher".id = "teacher_subject".teacher_id
-join subject on "teacher_subject".subject_id = subject.id group by "teacher".id having count("subject".name) > 1;
+         select  teacher.first_name ,teacher.last_name , count(*)
+        from teacher
+        join teacher_subject on "teacher".id = "teacher_subject".teacher_id
+        join subject on "teacher_subject".subject_id = subject.id group by "teacher".id having count("subject".name)  > 1;
 
 end;
 $$
 Language plpgsql
+-- count("subject".name)
 
 
 --  select 
